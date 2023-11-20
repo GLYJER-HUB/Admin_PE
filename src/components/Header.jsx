be58f6e6960a6109a53901da00ff70c51438bae0
itzmars <marsdoulens@proton.me>
2023-11-20 03:49:26 -0800

    first header commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import { colors } from '../utilities/colors';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import { Typography } from '@mui/material';
 
 
-const Header = () => {
-  return (
-    <div>header</div>
-  )
+
+function Header() {
+    return (
+        <>
+            <AppBar style={{ background: "#ffffff", boxShadow: '5px 1px #BE7B2E', }}>
+                <Toolbar >
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: colors.primary }}>
+                        Exploration de Projet
+                    </Typography>
+                    <div>
+                        <Box sx={{ display: "flex" }}>
+                            <Typography sx={{ color: colors.primary }}>
+                                John Doe
+                            </Typography>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                color="inherit">
+                                <AccountCircle sx={{ color: colors.primary }} />
+                            </IconButton>
+                        </Box>
+
+
+
+
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
 }
 
 export default Header
---
+++ src/utilities/colors.js
@@ -0,0 +1,6 @@
+
+
+export const colors = {
+    primary : "#05653F",
+    secondary: "#BE7B2E",
+};
