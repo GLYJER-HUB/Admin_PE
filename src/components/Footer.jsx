7d1202ae39f3c12edeacff3dd90fd03b20f1ff2e
whis99 <hanswhisler@gmail.com>
2023-11-22 08:46:12 -0500

    creating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Container, Grid, Typography } from '@mui/material';
+import { colors } from '../utilities/colors';
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Container component="footer" 
+      sx={{ 
+      bgcolor: colors.green, 
+      py: 2 
+      }}>
+
+      <Grid container justifyContent="space-between" alignItems="center">
+        <Grid item>
+          <Typography variant="body2" color={colors.white} fontWeight="bold">
+            &copy; TOUS DROITS RÉSERVÉS 2015 - 2023 UNIVERSITÉ ESPOIR
+          </Typography>
+          <Typography variant="body2" color={colors.white} fontWeight="bold">
+          UN MINISTÈRE DE CALVARY CHAPEL PORT-AU-PRINCE
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body2" color={colors.white} fontWeight="bold">
+            DEVELOPPÉ PAR  ZOOMER DIGITAL
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
--- src/utilities/colors.js
+++ src/utilities/colors.js
@@ -3,4 +3,6 @@ export const colors = {
     primary : "#05653F",
     secondary: "#BE7B2E",
     third: "#6D6C6C",
+    green: "#03653F",
+    white: "#FFFFFF"
 };
